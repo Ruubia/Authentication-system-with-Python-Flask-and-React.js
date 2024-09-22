d490e39fbb2dd327ada92f7f4ce3c115cfce4274
Beatriz Carmona Jurado <b.carmonaj@gmail.com>
2024-09-22 14:35:27 +0100

    Update home.js

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,22 +1,16 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import "../../styles/home.css";
+import Form from "../component/form";
 
 export const Home = () => {
-  const { store } = useContext(Context);
+	const { store, actions } = useContext(Context);
 
-  return (
-    <div className="text-center mt-5">
-      {store.user ? (
-        <>
-          <h1>Welcome, {store.user.email}!</h1>
-          <p>Great, now you can access the private area.</p>
-        </>
-      ) : (
-        <>
-          <h1>Please log in to see your dashboard</h1>
-        </>
-      )}
-    </div>
-  );
+	return (
+		<div className="text-center mt-5">
+		
+			<Form/>
+			
+		</div>
+	);
 };
