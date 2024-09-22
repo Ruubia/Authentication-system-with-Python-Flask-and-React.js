4e26501ef257c548ef1b51b49a18bd47561c64f5
Beatriz Carmona Jurado <b.carmonaj@gmail.com>
2024-09-22 12:53:33 +0100

    Update home.js

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,26 +1,22 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
 export const Home = () => {
-	const { store, actions } = useContext(Context);
+  const { store } = useContext(Context);
 
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
-			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://start.4geeksacademy.com/starters/react-flask">
-					Read documentation
-				</a>
-			</p>
-		</div>
-	);
+  return (
+    <div className="text-center mt-5">
+      {store.user ? (
+        <>
+          <h1>Welcome, {store.user.email}!</h1>
+          <p>Great, now you can access the private area.</p>
+        </>
+      ) : (
+        <>
+          <h1>Please log in to see your dashboard</h1>
+        </>
+      )}
+    </div>
+  );
 };
