8c6269fdad803e47be3d20ab70acac3b3372046e
Beatriz Carmona Jurado <b.carmonaj@gmail.com>
2024-09-22 14:30:59 +0100

    Update models.py

--- src/api/models.py
+++ src/api/models.py
@@ -6,21 +6,16 @@ db = SQLAlchemy()
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    password_hash = db.Column(db.String(512), nullable=False)
-
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
-
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
-
+    password = db.Column(db.String(80), unique=False, nullable=False)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    
     def __repr__(self):
-        return f'<User {self.username}>'
+
+        return f'<User {self.email}>'
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
-            "username": self.username
+            # do not serialize the password, its a security breach
         }
