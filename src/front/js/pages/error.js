a32b0fbd589aac485a2ba4aaf55135ca0607ab5b
Beatriz Carmona Jurado <b.carmonaj@gmail.com>
2024-09-22 12:52:04 +0100

    Create error.js

---
+++ src/front/js/pages/error.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Error = () => {
+  return (
+    <div className="container mt-5">
+      <h1>Restricted Access</h1>
+      <p>You do not have permission to view this page. Please log in.</p>
+    </div>
+  );
+};
