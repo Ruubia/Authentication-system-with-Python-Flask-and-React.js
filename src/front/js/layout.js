72259e85db22147b5064bcbd4ed49c002184f199
Beatriz Carmona Jurado <b.carmonaj@gmail.com>
2024-09-22 12:56:58 +0100

    Update layout.js

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -1,40 +1,68 @@
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+// src/front/js/layout.js
+
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 import { BackendURL } from "./component/backendURL";
 
 import { Home } from "./pages/home";
 import { Demo } from "./pages/demo";
 import { Single } from "./pages/single";
+import { Signup } from "./pages/signup";
+import { Private } from "./pages/private";
+import { Error } from "./pages/error";
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
 
-//create your first component
-const Layout = () => {
-    //the basename is used when your project is published in a subdirectory and not in the root of the domain
-    // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
-    const basename = process.env.BASENAME || "";
+const App = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-    if(!process.env.BACKEND_URL || process.env.BACKEND_URL == "") return <BackendURL/ >;
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    setIsAuthenticated(!!token);
+  }, []);
 
-    return (
-        <div>
-            <BrowserRouter basename={basename}>
-                <ScrollToTop>
-                    <Navbar />
-                    <Routes>
-                        <Route element={<Home />} path="/" />
-                        <Route element={<Demo />} path="/demo" />
-                        <Route element={<Single />} path="/single/:theid" />
-                        <Route element={<h1>Not found!</h1>} />
-                    </Routes>
-                    <Footer />
-                </ScrollToTop>
-            </BrowserRouter>
-        </div>
-    );
+  const handleLoginSuccess = () => {
+    setIsAuthenticated(true);
+  };
+
+  const handleLogout = () => {
+    setIsAuthenticated(false);
+  };
+
+  const basename = process.env.BASENAME || "";
+
+  if (!process.env.BACKEND_URL || process.env.BACKEND_URL === "")
+    return <BackendURL />;
+
+  return (
+    <div>
+      <Router basename={basename}>
+        <ScrollToTop>
+          <Navbar
+            isAuthenticated={isAuthenticated}
+            onLogout={handleLogout}
+            onLoginSuccess={handleLoginSuccess}
+          />
+          <Routes>
+            <Route element={<Home />} path="/" />
+            <Route element={<Demo />} path="/demo" />
+            <Route element={<Single />} path="/single/:theid" />
+            <Route element={<Signup />} path="/signup" />
+            <Route
+              element={<Private isAuthenticated={isAuthenticated} />}
+              path="/private"
+            />
+            <Route element={<Error />} path="/error" />
+            <Route element={<h1>Not found!</h1>} />
+          </Routes>
+          <Footer />
+        </ScrollToTop>
+      </Router>
+    </div>
+  );
 };
 
-export default injectContext(Layout);
+export default injectContext(App);
